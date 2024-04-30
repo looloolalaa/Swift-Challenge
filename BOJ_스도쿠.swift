19f9187aa2230148dcd9d05b32272c64afdde2ad
Kihyun Lee <dlrlgus65@nate.com>
2024-04-30 10:35:51 +0900

    0일때 놓을 수 있는 숫자들

--- BOJ_스도쿠.swift
+++ BOJ_스도쿠.swift
@@ -1,65 +1,59 @@
-// 0인 놈들만 dfs + 백트래킹
+// 0일때 놓을 수 있는 숫자들
 import Foundation
 
-var table: [[Int]] = []
-for _ in 0..<9 {
-    table.append(readLine()!.components(separatedBy: " ").map { Int($0)! })
-}
-
-var blanks: [[Int]] = []
-for i in 0..<9 {
-    for j in 0..<9 {
-        if table[i][j] == 0 {
-            blanks.append([i, j])
-        }
-    }
-}
-
-func valid(_ i: Int, _ j: Int, _ k: Int) -> Bool {
-    for b in 0..<9 {
-        if table[i][b] == k {
+func valid(_ i: Int, _ j: Int, _ n: Int) -> Bool {
+    for y in 0..<9 {
+        if arr[i][y] == n {
             return false
         }
     }
-    for a in 0..<9 {
-        if table[a][j] == k {
+    for x in 0..<9 {
+        if arr[x][j] == n {
             return false
         }
     }
-
-    let (ni, nj) = ((i/3)*3, (j/3)*3)
-    for a in ni..<(ni+3) {
-        for b in nj..<(nj+3) {
-            if table[a][b] == k {
+    let (a, b) = (i/3*3, j/3*3)
+    for x in 0..<3 {
+        for y in 0..<3 {
+            if arr[a+x][b+y] == n {
                 return false
             }
         }
     }
-
+    
     return true
 }
 
-func dfs(_ i: Int) {
-    if i == blanks.count {
-        for t in table {
-            for a in t {
-                print(a, terminator: " ")
-            }
-            print()
+var arr: [[Int]] = []
+for _ in 0..<9 {
+    arr.append(Array(readLine()!).map { Int(String($0))! })
+}
+
+func dfs(_ idx: Int) {
+    if idx == 81 {
+        for a in arr {
+            print(a.map { String($0) }.joined(separator: ""))
         }
         exit(0)
     }
-
-    let (x, y) = (blanks[i][0], blanks[i][1])
-    for k in 1...9 {
-        if valid(x, y, k) {
-            table[x][y] = k
-            dfs(i + 1)
-            table[x][y] = 0
+    
+    let (i, j) = (idx/9, idx%9)
+    if arr[i][j] != 0 {
+        dfs(idx+1)
+    } else {
+        for n in 1...9 {
+            if valid(i, j, n) {
+                arr[i][j] = n
+                dfs(idx+1)
+                arr[i][j] = 0
+            }
         }
     }
+    
 }
 
 dfs(0)
+
+
 
 
