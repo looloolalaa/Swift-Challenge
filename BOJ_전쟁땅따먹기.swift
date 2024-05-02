4ec347d7d2985f245ba4663f2706790669d52609
Kihyun Lee <dlrlgus65@nate.com>
2024-05-02 11:22:18 +0900

    fIO.readInt()

---
+++ BOJ_전쟁땅따먹기.swift
@@ -0,0 +1,77 @@
+// fIO.readInt()
+import Foundation
+
+final class FileIO {
+    private var buffer:[UInt8]
+    private var index: Int
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        
+        buffer = Array(fileHandle.readDataToEndOfFile())+[UInt8(0)] // 인덱스 범위 넘어가는 것 방지
+        index = 0
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer { index += 1 }
+
+        return buffer.withUnsafeBufferPointer { $0[index] }
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+            || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45{ isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+    @inline(__always) func readString() -> Int {
+        var str = 0
+        var now = read()
+
+        while now == 10
+            || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+        while now != 10
+            && now != 32 && now != 0 {
+                str += Int(now)
+                now = read()
+        }
+        
+        return str
+    }
+}
+
+
+let fIO = FileIO()
+for _ in 0..<fIO.readInt() {
+    let n = fIO.readInt()
+    var line: [Int] = []
+    for _ in 0..<n {
+        line.append(fIO.readInt())
+    }
+    
+    var counter: [Int: Int] = [:]
+    for i in line {
+        counter[i, default: 0] += 1
+    }
+    
+    var found = false
+    for (k, v) in counter {
+        if v > (n/2) {
+            print(k)
+            found = true
+        }
+    }
+    
+    if !found { print("SYJKGW") }
+    
+}
