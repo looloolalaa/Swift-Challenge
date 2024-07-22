8dd33999787597e3652956910f44f20fa8f100f4
Kihyun Lee <dlrlgus65@nate.com>
2024-07-22 16:02:45 +0900

    타일링 규칙 찾기

---
+++ BOJ_2xn타일링.swift
@@ -0,0 +1,12 @@
+// 타일링 규칙 찾기
+let n = Int(readLine()!)!
+var dp = Array(repeating: 0, count: 1001)
+dp[1] = 1
+dp[2] = 2
+
+for i in 3...1000 {
+    dp[i] = (dp[i-1] + dp[i-2]) % 10007
+}
+print(dp[n])
+
+
