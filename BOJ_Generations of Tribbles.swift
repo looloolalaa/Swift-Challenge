07367a916840d7b3d7aa0b4185be44c6b09d0492
Kihyun Lee <dlrlgus65@nate.com>
2026-01-23 17:55:26 +0900

    dp3

---
+++ BOJ_Generations of Tribbles.swift
@@ -0,0 +1,16 @@
+// dp3
+var dp = Array(repeating: 0, count: 68)
+dp[0] = 1
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+
+for i in 4...67 {
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3] + dp[i-4]
+}
+
+for _ in 0..<Int(readLine()!)! {
+    let n = Int(readLine()!)!
+    print(dp[n])
+}
+
